79dedc85ba4efc30dfc17f3834d2078f77b01c76
TommasoBertossi <tommasobertossi@gmail.com>
2023-09-11 16:47:39 +0200

    first commit

---
+++ intro-vite/vite-project/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import AppTitle from "./components/AppTitle.vue";
+import AppHero from "./components/AppHero.vue";
+
+export default {
+  components: { AppTitle, AppHero },
+};
+</script>
+
+<template>
+  <AppTitle />
+  <AppHero />
+</template>
+
+<style></style>
---
+++ intro-vite/vite-project/src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      image: "vue.svg",
+    };
+  },
+  methods: {
+    getImageUrl(name) {
+      return new URL("../assets/${name}", import.meta.url).href;
+    },
+  },
+};
+</script>
+<template>
+  <img :src="getImageUrl(image)" alt="" />
+  <img src="/vite.svg" alt="" />
+</template>
+
+<style></style>
